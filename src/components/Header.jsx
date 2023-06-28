a80739e9395ca8bdf7a8d338b41a03b86c3b33d1
Kapee87 <kapserker@gmail.com>
2023-06-28 10:05:49 -0300

    algun estilo al header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,12 +3,12 @@ import logo from '../assets/logo-ico.png';
 
 function Header() {
     return (
-        <header>
-            <nav>
+        <header className='bg-dark'>
+            <nav className='flex justify-between w-full fixed top-0 left-0 py-4 px-6 items-center bg-dark'>
                 <a href="#"><img src={logo} alt="logo" /></a>
                 <input type="checkbox" id="menu-toggle" />
                 <label htmlFor="menu-toggle" className="">&#9776;</label>
-                <ul>
+                <ul className='text-light block [&>li]:inline [&>li]:mx-4 [&>li]:bg-clip-text [&>li]:'>
                     <li><a href="#inicio">Inicio</a></li>
                     <li><a href="#soluciones">Soluciones</a></li>
                     <li><a href="#experiencia">Experiencia</a></li>
