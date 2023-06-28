f8532a2c57501a49bd6852c434e4f9966b2341d1
Tsu <lucanahtsu@gmail.com>
2023-06-28 13:01:11 -0300

    add header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,18 +2,17 @@ import React from 'react';
 import logo from '../assets/logo-ico.png';
 
 function Header() {
+    // TODO: Hamburguer menu
     return (
-        <header className='bg-dark'>
-            <nav className='flex justify-between w-full fixed top-0 left-0 py-4 px-6 items-center bg-dark'>
-                <a href="#"><img src={logo} alt="logo" /></a>
-                <input type="checkbox" id="menu-toggle" />
-                <label htmlFor="menu-toggle" className="">&#9776;</label>
-                <ul className='text-light block [&>li]:inline [&>li]:mx-4 [&>li]:bg-clip-text [&>li]:'>
-                    <li><a href="#inicio">Inicio</a></li>
-                    <li><a href="#soluciones">Soluciones</a></li>
-                    <li><a href="#experiencia">Experiencia</a></li>
-                    <li><a href="#team">Team</a></li>
-                    <li><a href="#contactanos">Contactános</a></li>
+        <header className="bg-dark">
+            <nav className="flex justify-between w-full fixed top-0 left-0 py-1 px-1 items-center bg-dark">
+                <a href="#"><img src={logo} alt="logo" className="h-12" /></a>
+                <ul className="space-x-2 md:space-x-4 md:mr-2 text-light [&>li]:inline md:[&>li>a]:font-bold">
+                    <li><a href="#inicio" className='text-light gradient-text-hover'>Inicio</a></li>
+                    <li><a href="#soluciones" className='text-light gradient-text-hover'>Soluciones</a></li>
+                    <li><a href="#experiencia" className='text-light gradient-text-hover'>Experiencia</a></li>
+                    <li><a href="#team" className='text-light gradient-text-hover'>Team</a></li>
+                    <li><a href="#contactanos" className='text-light gradient-text-hover'>Contactános</a></li>
                 </ul>
             </nav>
         </header>
