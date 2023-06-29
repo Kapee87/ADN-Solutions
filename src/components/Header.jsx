ab5af1b94f7a4471e34fa2017242c533824cd871
Kapee87 <kapserker@gmail.com>
2023-06-29 16:32:24 -0300

    otro guarde progress

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,21 +3,23 @@ import logo from '../assets/logo-ico.png';
 
 function Header() {
     const [isOpen, setIsOpen] = useState(false)
-    const burgerLine = `h-1 w-6 my-1 rounded-full bg-black transition ease transform duration-300`;
+    const burgerLine = `h-2 w-7 my-1 rounded-full bg-light transition ease transform duration-300`;
+    const navLinkStyleDesktop = 'text-light gradient-text-hover'
+
     // TODO: Hamburguer menu
     return (
         <header className="bg-dark">
             <nav className="flex justify-between w-full fixed top-0 left-0 py-1 px-1 items-center bg-dark">
                 <a href="#"><img src={logo} alt="logo" className="h-12" /></a>
-                <ul className="space-x-2 md:space-x-4 md:mr-2 text-light [&>li]:inline md:[&>li>a]:font-bold">
-                    <li><a href="#inicio" className='text-light gradient-text-hover'>Inicio</a></li>
-                    <li><a href="#soluciones" className='text-light gradient-text-hover'>Soluciones</a></li>
-                    <li><a href="#experiencia" className='text-light gradient-text-hover'>Experiencia</a></li>
-                    <li><a href="#team" className='text-light gradient-text-hover'>Team</a></li>
-                    <li><a href="#contactanos" className='text-light gradient-text-hover'>Contactános</a></li>
+                <ul className="space-x-2 md:space-x-4 md:mr-2 text-light [&>li]:inline md:[&>li>a]:font-bold hidden lg:inline-block ">
+                    <li><a href="#inicio" className={navLinkStyleDesktop}>Inicio</a></li>
+                    <li><a href="#soluciones" className={navLinkStyleDesktop}>Soluciones</a></li>
+                    <li><a href="#experiencia" className={navLinkStyleDesktop}>Experiencia</a></li>
+                    <li><a href="#team" className={navLinkStyleDesktop}>Team</a></li>
+                    <li><a href="#contactanos" className={navLinkStyleDesktop}>Contactános</a></li>
                 </ul>
                 <button
-                    className="h-10 w-10 border-2 border-light rounded flex flex-col justify-center items-center group bg-light lg:hidden "
+                    className="h-10 w-10 rounded flex flex-col justify-center items-center group bg-dark lg:hidden "
                     onClick={() => setIsOpen(!isOpen)}
                 >
                     <div
@@ -35,6 +37,9 @@ function Header() {
                             }`}
                     />
                 </button>
+                <div className={isOpen ? 'visible' : 'hidden'}>
+
+                </div>
             </nav>
         </header>
     );
