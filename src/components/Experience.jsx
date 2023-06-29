e535d2fc15ef2dd3a6366834deeaeeae347372b5
Tsu <lucanahtsu@gmail.com>
2023-06-28 22:41:35 -0300

    replace textGradient component for gradient-text tailwind custom property

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,13 +1,9 @@
-import TextGradient from "./TextGradient";
+import React from 'react';
 let arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 function Experience() {
     return (
         <section className="bg-dark flex justify-center items-center p-3">
-            <h2>
-                <TextGradient style="text-4xl">
-                    Experiencia
-                </TextGradient>
-            </h2>
+            <h2 className="gradient-text font-bold text-4xl">Experiencia</h2>
             <div>
                 {arr.map((item) => (
                     <a key={item.id}>
@@ -18,8 +14,6 @@ function Experience() {
                     </a>
                 ))}
             </div>
-
-
         </section>
     )
 };
