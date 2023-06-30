c582a533939d1a71b0f1c98990a9f1ac00b2e106
Franco Miguez <franco.miguez98@gmail.com>
2023-06-30 03:07:13 -0300

    End section Experience add responsive and style

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 let arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 function Experience() {
     return (
-        <section className="bg-dark flex justify-center items-center p-3">
-            <h2 className="gradient-text font-bold text-5xl -rotate-90">Experiencia</h2>
-            <div className="flex flex-col">
-                {arr.map((item) => (
-                    <a key={item.id} className='relative m-10 hover:scale-105 transition-transform duration-300'>
-                        <img src="https://placehold.co/600x400" alt="proyecto1" className='w-80'/>
+        <section className="bg-dark flex flex-col lg:flex-row justify-center items-center px-3 py-5">
+            <h2 className="gradient-text font-bold text-5xl mb-4 lg:mb-0 lg:-rotate-90">Experiencia</h2>
+            <div className="flex flex-wrap justify-center gap-4">
+                {arr.map((_,item) => (
+                    <a key={item} className='relative hover:scale-105 transition-transform duration-300'>
+                        <img src="https://placehold.co/600x400" alt="proyecto1" className='w-72 h-48 object-cover'/>
                         <div className='absolute top-0 left-0 w-full h-full flex justify-center items-center bg-black/[0.9] opacity-0 hover:opacity-100 border border-light'>
-                            <h3 className='text-2xl text-light'>Proyecto</h3>
+                            <h3 className='text-2xl text-light font-primary font-bold'>Proyecto</h3>
                         </div>
                     </a>
                 ))}
