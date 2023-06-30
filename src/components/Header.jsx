509f1f6f9fac70ea439cd65f79f4450435adca25
Kapee87 <kapserker@gmail.com>
2023-06-30 14:39:43 -0300

    menu dropdown que desaparece al hacer click en un link o en alguna parte del mismo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,16 @@
-import React, { useState } from 'react';
+import React from 'react';
 import logo from '../assets/logo-ico.png';
+import { useNavbar } from '../services/useNavbar';
 
 function Header() {
-    const [isOpen, setIsOpen] = useState(false)
-    const [page, setPage] = useState('home')
+    const { isOpen, setIsOpen } = useNavbar()
 
     const burgerLine = `h-2 w-7 my-1 rounded-full bg-light transition ease transform duration-300`;
     const navLinkStyleDesktop = 'text-light gradient-text-hover font-bold hover:drop-shadow-[0_35px_35px_rgba(0,0,0,0.25)] hover:border '
-    
+
     // TODO: Hamburguer menu
     return (
-        <header className="bg-dark">
+        <header className="bg-dark" onClick={() => { isOpen ? setIsOpen(false) : '' }}>
             <nav className="flex justify-between min-w-full fixed top-0 left-0 py-1 px-1 items-center bg-dark">
                 <a href="#"><img src={logo} alt="logo" className="h-12" /></a>
                 <ul className="space-x-2 md:space-x-4 md:mr-2 text-light [&>li]:inline hidden lg:inline-block ">
@@ -42,25 +42,25 @@ function Header() {
                 <div className={`${isOpen ? 'grid' : 'hidden'} absolute bg-dark h-fit w-full top-full lg:hidden font-primary`}>
                     <ul className="flex flex-col gap-4 rounded-lg items-center transition-all">
                         <li>
-                            <a href="#" className={`${navLinkStyleDesktop} ${page === 'home' ? 'active' : ''}`}>Inicio</a>
+                            <a href="#inicio" className={navLinkStyleDesktop}>Inicio</a>
                         </li>
                         <li>
-                            <a href="#" className={`${navLinkStyleDesktop} ${page === 'not' ? 'active' : ''}`}>Soluciones</a>
+                            <a href="#soluciones" className={navLinkStyleDesktop} > Soluciones</a>
                         </li>
                         <li>
-                            <a href="#" className={`${navLinkStyleDesktop} ${page === 'home' ? 'active' : ''}`}>Experiencia</a>
+                            <a href="#experiencia" className={navLinkStyleDesktop}>Experiencia</a>
                         </li>
                         <li>
-                            <a href="#" className={`${navLinkStyleDesktop} ${page === 'home' ? 'active' : ''}`}>Team</a>
+                            <a href="#team" className={navLinkStyleDesktop}>Team</a>
                         </li>
                         <li>
-                            <a href="#" className={`${navLinkStyleDesktop} ${page === 'home' ? 'active' : ''}`}>Contactános</a>
+                            <a href="#contactanos" className={navLinkStyleDesktop}>Contactános</a>
                         </li>
                     </ul>
 
                 </div>
             </nav>
-        </header>
+        </header >
     );
 }
 
